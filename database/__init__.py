53945d70fc71e2462b043c75cc0ca3b4051c19fd
Javlon_mac <richk880@gmail.com>
2023-09-28 19:46:31 +0500

    full project

--- database/__init__.py
+++ database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@db/postgres'
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
